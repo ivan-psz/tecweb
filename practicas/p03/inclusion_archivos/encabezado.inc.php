eaf220dfb81368b851eadb77fb8dec2564a5dc78
Ivan <ivan.perezs248@gmail.com>
2024-08-29 23:52:03 -0600

    Corrección de documentos php

--- practicas/p03/inclusion_archivos/encabezado.inc.php
+++ practicas/p03/inclusion_archivos/encabezado.inc.php
@@ -3,7 +3,7 @@
 <?php
 $variable1="PHP 5";
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang=“es">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <?php
@@ -11,11 +11,11 @@ echo "<title>Una página que contiene muchas inclusiones $variable1</title>";
 ?>
 </head>
 <body>
-<?php ← (1)
-$variableext=« Este texto proviene del archivo incluido";
+<?php 
+$variableext= "Este texto proviene del archivo incluido";
 echo "<div><h1 style=\"border-width:5;border-style:double;background-color:#ffcc99;\">
-Bienvenido en el sitio $variable1 </h1>"; ← (2)
+Bienvenido en el sitio $variable1 </h1>";
 echo "<h3> $variableext</h3>";
-echo "Nombre de archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp;&nbsp;"; ← (3)
-echo " Nombre del archivo incluido : ", __FILE__ ,"</div> "; ← (3)
+echo "Nombre de archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp;&nbsp;"; 
+echo " Nombre del archivo incluido : ", __FILE__ ,"</div> "; 
 ?>
