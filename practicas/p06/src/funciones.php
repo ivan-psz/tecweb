a76e69a4817a8428b011ed623c57037aad9560f6
Ivan <ivan.perezs248@gmail.com>
2024-09-18 23:59:36 -0600

    Corrección del ejercicio 1 en index.php y en funciones.php

--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -1,8 +1,8 @@
 <?php
 
-    //FUNCION DEL EJERCICIO 1
+    //FUNCIÓN DEL EJERCICIO 1
 
-    function esMultiplo5y7($number) {
+    function ejercicioUno($number) {
         if (is_numeric($number)) {
             if ($number % 5 == 0 && $number % 7 == 0) {
                 echo '<p><strong>Respuesta: El número '.$number.' SÍ es múltiplo de 5 y 7.</strong></p>';
@@ -12,6 +12,12 @@
         } else {
             echo '<p><strong>Por favor, ingrese un número válido.</strong></p>';
         }
+    }
+
+    //FUNCIÓN DEL EJERCICIO 2
+
+    function ejercicioDos(){
+        
     }
 
 ?>
