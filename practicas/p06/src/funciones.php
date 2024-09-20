f211d940047c3ebd83fe8b45d175c5014d094060
Ivan <ivan.perezs248@gmail.com>
2024-09-20 10:53:50 -0600

    Creación del ejercicio 5

--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -1,5 +1,20 @@
 <?php
 
+    function generarUpperXHTML($titulo){
+        echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">';
+        echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">';
+            echo "<head>";
+                echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+                echo '<title>' . $titulo . '</title>';
+            echo "</head>";
+            echo "<body>";
+    }
+
+    function generarLowerXHTML(){
+            echo "</body>";
+        echo "</html>";
+    }
+
     //FUNCIÓN DEL EJERCICIO 1
 
     function ejercicioUno($number) {
@@ -101,10 +116,49 @@
         echo '</table>';
     }
 
+    //FUNCIÓN DEL EJERCICIO 5
+
+    function ejercicioCinco($age, $sex, $titulo, $ban){
+        generarUpperXHTML($titulo);
+
+        echo '<div style="text-align: center;">';
+
+        if($ban){
+            if($age > 17 && $age < 36 && $sex == "F"){
+                echo "<p><strong>Bienvenida, usted está en el rango de edad permitido.</strong></p>";
+            }
+            else{
+                if($sex == "M"){
+                    echo "<p><strong>Disculpe, usted no está autorizado para acceder.</strong></p>";
+                }
+                else{
+                    echo "<p><strong>Disculpe, usted no está autorizada para acceder debido a que no entra en el rango de edad.</strong></p>";
+                }
+            }
+        }
+        else{
+            echo "<p><strong>Se introdujo un valor inválido para la edad. Vuelva a intentarlo</strong></p>";
+        }
+
+        echo '</div>';
+
+        generarLowerXHTML();
+    }
+
     if(isset($_POST['age']) && isset($_POST['sex'])){
         $age = $_POST['age'];
         $sex = $_POST['sex'];
+        $titulo = "Respuesta del servidor";
 
+        if (is_numeric($age) && $age > 0) {
+            $age = (int)$age;
+            $ban = true;
+        } 
+        else {
+            $ban = false;
+        }
+
+        ejercicioCinco($age, $sex, $titulo, $ban);
     }
 
 
