3ba265eeac9d02fe93905e2f08e35ac254b996d1
Ivan <ivan.perezs248@gmail.com>
2024-09-28 21:03:17 -0600

    Se certificó el archivo index.php nuevamente

--- practicas/actividad_xhtml/index.php
+++ practicas/actividad_xhtml/index.php
@@ -317,9 +317,8 @@
         </div>
 
         <p class="certificado">
-            <a href="https://validator.w3.org/markup/check?uri=referer">
-                <img src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" />
-            </a>
+            <a href="https://validator.w3.org/markup/check?uri=referer"><img
+            src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
         </p>
 
     </body>
