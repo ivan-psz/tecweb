4842097063ddf2dd7ccad155c656d1d77c797f7d
Ivan <ivan.perezs248@gmail.com>
2024-09-28 21:01:19 -0600

    Corrección de estilos

--- practicas/actividad_xhtml/index.php
+++ practicas/actividad_xhtml/index.php
@@ -316,7 +316,7 @@
             <br />
         </div>
 
-        <p>
+        <p class="certificado">
             <a href="https://validator.w3.org/markup/check?uri=referer">
                 <img src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" />
             </a>
