961ae292c44c0fca40027e6541c807f0373ccec6
Ivan <ivan.perezs248@gmail.com>
2024-10-07 22:14:36 -0600

    Creación de ejercicios 9 - 18

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -133,6 +133,167 @@ function ejemploOcho(){
     }
 }
 
+function ejemploNueve(){
+    var col;
+    
+    col = prompt('Ingresa el color con el que quieras pintar el fondo de la ventana (rojo, verde o azul): ', '');
+
+    switch(col){
+        case 'rojo':
+            document.bgColor = '#ff0000';
+            break;
+        case 'verde':
+            document.bgColor = '#00ff00';
+            break;
+        case 'azul':
+            document.bgColor = '#0000ff';
+            break;
+        default:
+            document.bgColor = '#FFFFFF';
+    }
+}
+
+function ejemploDiez(){
+    var x;
+    x = 1;
+    
+    while(x <= 100){
+        document.write(x);
+        document.write('<br />');
+        x = x + 1;
+    }
+}
+
+function ejemploOnce(){
+    var x = 1;
+    var suma = 0;
+    var valor;
+
+    while(x <= 5){
+        valor = prompt('Ingresa el valor: ', '');
+        valor = parseInt(valor);
+        suma = suma + valor;
+        x = x + 1;
+    }
+    document.write('La suma de los valores ingresados es: ' + suma + '<br/>');
+}
+
+function ejemploDoce(){
+    var valor;
+    do{
+        valor = prompt('Ingresa un valor entre 0 y 999: ', '');
+        valor = parseInt(valor);
+
+        document.write('El valor ' + valor + ' tiene: ');
+
+        if(valor < 10){
+            document.write('1 dígito.');
+        }
+        else{
+            if(valor < 100){
+                document.write('2 dígitos.');
+            }
+            else{
+                document.write('3 dígitos.');
+            }
+        }
+
+        document.write('<br/>');
+    }while(valor != 0);
+}
+
+function ejemploTrece(){
+    var f;
+    
+    for(f = 1; f <= 10; f++){
+        document.write(f + " ");
+    }
+}
+
+function ejemploCatorce(){
+    document.write("Cuidado <br/>");
+    document.write("Ingresa tu documento correctamente <br/>");
+    document.write("Cuidado <br/>");
+    document.write("Ingresa tu documento correctamente <br/>");
+    document.write("Cuidado <br/>");
+    document.write("Ingresa tu documento correctamente <br/>");
+}
+
+function mostrarMensaje(){
+    document.write("Cuidado <br/>");
+    document.write("Ingresa tu documento correctamente <br/>");
+}
+
+function ejemploQuince(){
+    mostrarMensaje();
+    mostrarMensaje();
+    mostrarMensaje();
+}
+
+function mostrarRango(x1, x2){
+    var inicio;
+
+    for(inicio = x1; inicio <= x2; inicio++){
+        document.write(inicio + ' ');
+    }
+}
+
+function ejemploDieciseis(){
+    var valor1, valor2;
+
+    valor1 = prompt('Ingresa el valor inferior: ', '');
+    valor1 = parseInt(valor1);
+    valor2 = prompt('Ingresa el valor superior: ', '');
+    valor2 = parseInt(valor2);
+
+    mostrarRango(valor1, valor2);
+}
+
+function convertirCastellano(x){
+    if(x == 1)
+        return "uno";
+    else
+        if(x == 2)
+            return "dos";
+        else
+            if(x == 3)
+                return "tres";
+            else
+                if(x == 4)
+                    return "cuatro";
+                else
+                    if(x == 5)
+                        return "cinco";
+                    else
+                        return "valor incorrecto";
+}
+
+function ejemploDiecisiete(){
+    var valor = prompt('Ingresa un valor entre 1 y 5: ', '');
+    valor = parseInt(valor);
+    var r = convertirCastellano(valor);
+    document.write(r);
+}
+
+function convertirCastellanoDos(x){
+    switch(x){
+        case 1: return "uno";
+        case 2: return "dos";
+        case 3: return "tres";
+        case 4: return "cuatro";
+        case 5: return "cinco";
+        default: return "valor incorrecto";
+    }
+}
+
+function ejemploDieciocho(){
+    var valor = prompt('Ingresa un valor entre 1 y 5: ', '');
+    valor = parseInt(valor);
+    var r = convertirCastellanoDos(valor);
+    document.write(r);
+}
+
+/*
 function getDatos(){
     var nombre = prompt('Nombre: ', 'Usuario');
     var edad = prompt('Edad: ', 0);
@@ -146,3 +307,4 @@ function getDatos(){
     var div2 = document.getElementById('edad');
     div2.innerHTML = '<h3>Edad: ' + edad + '</h3>';
 }
+*/
