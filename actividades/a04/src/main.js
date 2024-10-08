3ddd33bb6b218e2c850b2688e0090b74dd4dc707
Ivan <ivan.perezs248@gmail.com>
2024-10-07 19:38:25 -0600

    Creación de funciones de ejercicios 1 - 8

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -47,6 +47,92 @@ function ejemploCuatro(){
     document.write(producto);
 }
 
+function ejemploCinco(){
+    var nombre;
+    var nota;
+
+    nombre = prompt('Ingresa tu nombre: ', '');
+    nota = prompt('Ingresa tu nota: ', '');
+
+    if(nota >= 4){
+        document.write(nombre + ' está aprobado con un ' + nota);
+    }
+}
+
+function ejemploSeis(){
+    var num1, num2;
+
+    num1 = prompt('Ingresa el primer número: ', '');
+    num2 = prompt('Ingresa el segundo número: ', '');
+
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    if(num1 > num2){
+        document.write('El mayor es: ' + num1);
+    }
+    else{
+        document.write('El mayor es: ' + num2);
+    }
+}
+
+function ejemploSiete(){
+    var nota1, nota2, nota3;
+
+    nota1 = prompt('Ingresa la 1ra. nota: ', '');
+    nota2 = prompt('Ingresa la 2da. nota: ', '');
+    nota3 = prompt('Ingresa la 3ra. nota: ', '');
+
+    //Convertimos los 3 strings en enteros
+
+    nota1 = parseInt(nota1);
+    nota2 = parseInt(nota2);
+    nota3 = parseInt(nota3);
+
+    var pro;
+
+    pro = (nota1 + nota2 + nota3) / 3;
+
+    if(pro >= 7){
+        document.write('Aprobado con ' + parseInt(pro));
+    }
+    else{
+        if(pro >= 4){
+            document.write('Regular con ' + parseInt(pro));
+        }
+        else{
+            document.write('Reprobado' + parseInt(pro));
+        }
+    }
+}
+
+function ejemploOcho(){
+    var valor;
+
+    valor = prompt('Ingresa un valor comprendido entre 1 y 5: ', '');
+    valor = parseInt(valor);
+
+    switch(valor){
+        case 1:
+            document.write('Ingresaste un uno');
+            break;
+        case 2:
+            document.write('Ingresaste un dos');
+            break;
+        case 3:
+            document.write('Ingresaste un tres');
+            break;
+        case 4:
+            document.write('Ingresaste un cuatro');
+            break;
+        case 5:
+            document.write('Ingresaste un cinco');
+            break;
+        default:
+            document.write('Debes ingresar un valor comprendido entre 1 y 5');
+    }
+}
+
 function getDatos(){
     var nombre = prompt('Nombre: ', 'Usuario');
     var edad = prompt('Edad: ', 0);
