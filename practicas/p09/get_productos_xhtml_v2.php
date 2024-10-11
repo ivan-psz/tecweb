6f74f1bd40d493ec9d53d37c64c5d81a8839e795
Ivan <ivan.perezs248@gmail.com>
2024-10-11 02:09:34 -0600

    Creación de archivos para ejercicio 2 y 3

---
+++ practicas/p09/get_productos_xhtml_v2.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+    <?php
+    $data = array();
+	if(isset($_GET['tope']))
+    {
+		$tope = $_GET['tope'];
+    }
+    else
+    {
+        die('Parámetro "tope" no detectado...');
+    }
+
+	if (!empty($tope))
+	{
+		/** SE CREA EL OBJETO DE CONEXION */
+		@$link = new mysqli('localhost', 'root', '.YcPHLGg]QCW-fX/', 'marketzone');	
+
+		/** comprobar la conexión */
+		if ($link->connect_errno) 
+		{
+			die('Falló la conexión: '.$link->connect_error.'<br/>');
+			    /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
+		}
+
+		$link->set_charset('utf8');
+		
+		/** Crear una tabla que no devuelve un conjunto de resultados */
+		if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") ) 
+		{
+            /** Se extraen las tuplas obtenidas de la consulta */
+			$row = $result->fetch_all(MYSQLI_ASSOC);
+
+			/** útil para liberar memoria asociada a un resultado con demasiada información */
+			$result->free();
+		}
+
+		$link->close();
+	}
+	?>
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Productos con unidades menores a <?= $tope ?></title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	</head>
+	<body>
+		<h3>PRODUCTOS</h3>
+
+		<br/>
+		
+		<?php if( isset($row) && !empty($row)) : ?>
+
+			<table class="table">
+				<thead class="thead-dark">
+					<tr>
+					<th scope="col">#</th>
+					<th scope="col">Nombre</th>
+					<th scope="col">Marca</th>
+					<th scope="col">Modelo</th>
+					<th scope="col">Precio</th>
+					<th scope="col">Unidades</th>
+					<th scope="col">Detalles</th>
+					<th scope="col">Imagen</th>
+					</tr>
+				</thead>
+				<tbody>
+                    <?php
+                        foreach($row as $num => $registro){
+                            echo '<tr>';
+                            foreach($registro as $key => $value){
+                                if($key != 'imagen'){
+                                    if($key == 'id'){
+                                        echo '<th scope="row">' . $data[$num][$key] = utf8_encode($value) . '</th>';
+                                    }
+                                    else{
+                                        echo '<td>' . $data[$num][$key] = utf8_encode($value) . '</td>';
+                                    }
+                                }
+                                else{
+                                    echo '<td>';
+                                         echo '<img src = ' . $value . '>';
+                                    echo '</td>';
+                                }
+                            }
+                            echo '</tr>';
+                        }
+                    ?>
+				</tbody>
+			</table>
+        <?php else : ?>
+
+			 <script>
+                alert('No existen productos con unidades menores al tope introducido');
+             </script>
+
+		<?php endif; ?>
+	</body>
+</html>
