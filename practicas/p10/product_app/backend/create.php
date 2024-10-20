7ec0df17bc8c57161ee12ad7b5fa049464bda3be
Ivan <ivan.perezs248@gmail.com>
2024-10-19 20:01:11 -0600

    Corrección del ejercicio 3

--- practicas/p10/product_app/backend/create.php
+++ practicas/p10/product_app/backend/create.php
@@ -5,17 +5,18 @@
     if(!empty($producto)){
         $jsonObj = json_decode($producto);
 
-        echo 'Objeto recibido:' . "\n" . var_dump($jsonObj);
+        echo "Objeto recibido: \n"; 
+        echo var_dump($jsonObj);
 
         if(is_null($jsonObj)){
-            echo "\nError en el formato del JSON";
+            echo "\n\nError en el formato del JSON";
             exit;
         }
         else{
             $query = "SELECT * FROM productos WHERE nombre = '{$jsonObj->nombre}' AND eliminado = 0";
             if($result = $conexion->query($query)){
                 if($result->num_rows > 0){
-                    echo "\nEl producto ya existe dentro de la base de datos";
+                    echo "\n\nEl producto ya existe dentro de la base de datos";
                 }
                 else{
                     $nombre = $jsonObj->nombre;
@@ -28,15 +29,16 @@
 
                     $query = "INSERT INTO productos (nombre, marca, modelo, precio, detalles, unidades, imagen) VALUES ('{$nombre}', '{$marca}', '{$modelo}', '{$precio}', '{$detalles}', '{$unidades}', '{$ruta}')";
                     if($conexion->query($query)){
-                        echo "\nProducto insertado";
+                        echo "\n\nProducto insertado";
                     }
                     else{
-                        echo "\nError al insertar el producto";
+                        echo "\n\nError al insertar el producto";
                     }
                 }
                 $result -> free();
                 $conexion->close();
-                echo "\n" . '[SERVIDOR] Nombre: ' . $jsonObj -> nombre;
+                echo "\n\n";
+                echo '[SERVIDOR] Nombre: ' . $jsonObj -> nombre;
             }
             else{
                 die('Error en la consulta: ' . mysqli_error($conexion));
